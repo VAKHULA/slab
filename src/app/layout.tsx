e97ed623305f8d9885b262eedc6d44edb6639c36
VAKHULA <andrii.vakhula@gmail.com>
2025-07-20 06:08:02 +0200

    upd meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata } from "next";
-import localFont from 'next/font/local'
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
 
 const baseFont = localFont({
   src: [
@@ -25,11 +25,11 @@ const baseFont = localFont({
       style: 'italic',
     },
   ],
-})
+});
 
 export const metadata: Metadata = {
-  title: "Slab App",
-  description: "Slab App",
+  title: 'Slab App',
+  description: 'Slab App',
 };
 
 export default function RootLayout({
@@ -39,11 +39,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${baseFont.className} app antialiased dark`}
-      >
-        {children}
-      </body>
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+      <body className={`${baseFont.className} app antialiased dark`}>{children}</body>
     </html>
   );
 }
