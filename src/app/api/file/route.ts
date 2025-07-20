cf4f91f2832b560905e331e46dff6dfcabbea549
VAKHULA <andrii.vakhula@gmail.com>
2025-07-20 18:10:05 +0200

    wip

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -1,24 +1,24 @@
-import { put, list } from '@vercel/blob';
-import { NextResponse } from 'next/server';
+// import { put, list } from '@vercel/blob';
+// import { NextResponse } from 'next/server';
 
-export async function POST(request: Request): Promise<NextResponse> {
-  const { searchParams } = new URL(request.url);
-  const filename = searchParams.get('filename');
+// export async function POST(request: Request): Promise<NextResponse> {
+//   const { searchParams } = new URL(request.url);
+//   const filename = searchParams.get('filename');
 
-  // ⚠️ The below code is for App Router Route Handlers only
-  const blob = await put(filename, request.body, {
-    access: 'public',
-  });
+//   // ⚠️ The below code is for App Router Route Handlers only
+//   const blob = await put(filename, request.body, {
+//     access: 'public',
+//   });
 
-  // Here's the code for Pages API Routes:
-  // const blob = await put(filename, request, {
-  //   access: 'public',
-  // });
+//   // Here's the code for Pages API Routes:
+//   // const blob = await put(filename, request, {
+//   //   access: 'public',
+//   // });
 
-  return NextResponse.json(blob);
-}
+//   return NextResponse.json(blob);
+// }
 
-export async function GET(request: Request) {
-  const { blobs } = await list();
-  return Response.json(blobs);
-}
+// export async function GET(request: Request) {
+//   const { blobs } = await list();
+//   return Response.json(blobs);
+// }
